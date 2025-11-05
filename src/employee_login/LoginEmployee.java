7d6c196d9138d9b73b20e71cc3109d1d987f5c20
Ever-VC <ever.vasquezc27@gmail.com>
2025-11-05 07:39:35 -0600

    ➖ Removing Dependency:  Remuve unused dependencies

--- src/employee_login/LoginEmployee.java
+++ src/employee_login/LoginEmployee.java
@@ -2,16 +2,11 @@ package employee_login;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import java.time.Duration;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.*;
 
 public class LoginEmployee {
 	
@@ -34,6 +29,6 @@ public class LoginEmployee {
         WebElement panelEmpleado = driver.findElement(By.xpath("//h2[contains(text(),'Panel del Empleado')]"));
         Assert.assertNotNull(panelEmpleado);
         System.out.println("Se encontró el panel de empleado: " + panelEmpleado.getText());
-    } 
+    }
 
 }
